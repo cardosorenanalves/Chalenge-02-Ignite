7a3cd0ed3fcb9adf843375386f9333303b39a3c7
cardosorenanalves <renancardoso0307@gmail.com>
2022-04-14 14:47:47 -0300

    Projeto finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,48 @@
-export function Content() {
-  // Complete aqui
+
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentMovie {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+  
+}
+
+export function Content(props: ContentMovie) {
+  
+ 
+
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map((movie:any) => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
